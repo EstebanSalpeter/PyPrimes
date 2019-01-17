1ef8497a45ad0ea1052a502e513dffd1106644d1
Esteban <44044253+EstebanSalpeter@users.noreply.github.com>
2019-01-17 19:49:39 -0300

    Optimization and parameters check
    Best use of sets and its methods instead of lists.
    Requires still optimization in the mathematical determination of mx and mn

--- pyprimes.py
+++ pyprimes.py
@@ -2,36 +2,57 @@
 #author's email: esteban.salpeter@yahoo.com.ar
 def primes_between(min,max):
 #init
-	mx = round(max/2,0)+1
-	mn = round(min/2,0)-1
-	m=1
-	listado=[]
-	p=[]
-  
-	while (m<=(mx+1)*2/3):
-		l=m
-		while l<=((mx)*2)/3:
-    #my super formula
-			n=m*l*2+m+l
-			if n>mx:
-				break
-			if (n<=mx) & (n>=mn):
-				listado.append(n)
-			l+=1
-		m+=1
-	listado.sort()
-	tmp=listado[0]
-  #my super faster algorithm (to be improved)
-	for e in listado:
-		if e-tmp>1:
-			l=1
-			while l<e-tmp:
-				if (tmp+l)*2+1<max:
-					p.append((tmp+l)*2+1)
-				l+=1
-		tmp=e
-	return p
+        #parameters check
+        if min>max:
+                print ('Error: min parameter must be lower than the max one')
+                return 0
+        #the use of a set prevents duplicates
+        factorized=set()
+        #method
+        #we'll produce all odd numbers in range
+        #then we'll produce all non-prime odd numbers in range
+        #finally, we'll obtain all prime numbers by difference of both sets
+        
+        #definitions
+        #n: half-lower-bound of a non-prime odd number
+        #m, l: ordered pair (m>=l) used in my algorithm, derived from the assumption of the form (2*n+1) for any odd non-prime number
+        #mx: maximum value for m and l to produce max when m=l
+        #mn: minimum value of n to produce min (whatever values for m and l)
+        
+        #let's make only the necessary range by factorization
+        mx,mn = int((max-1)/2), int((min-1)/2)
+        m=1
+        if min%10 in [2,4,6,8,0]:
+                min+=1
+        if max%10 in [2,4,6,8,0]:
+                max-=1
+                
+        alloddsinrange=set(range(min,max+1,2))
+        
+        #uncomment line below for testing purposes
+        #print(alloddsinrange)
 
+        #let's start filling and factorizing!
+        while m<=mx:
+                l=1
+                while l<=m:
+                        #my super formula! generator of every nonprime odd number's half-lower-bound
+                        n=m*l*2+m+l
+                        if n>mx:
+                                break
+                        if (n<=mx) & (n>=mn):
+                                factorized.add(n*2+1)
+                        l+=1
+                m+=1
+                
+        #uncomment line below for testing purposes
+        #print(factorized)
+                
+        #let's use the best of Python collections!
+        #use this line if you want to stay using sets instead of lists
+        primes=alloddsinrange.difference(factorized)
+        #primes=sorted(alloddsinrange.difference(factorized))
+        return primes
 
 def is_prime(num):
     last = num%10
