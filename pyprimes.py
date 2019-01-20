999fd3278f1e532535bd09420cdb9cf8d1d5cad6
Esteban <44044253+EstebanSalpeter@users.noreply.github.com>
2019-01-19 23:15:11 -0300

    Added strategy choice: loop or difference

--- pyprimes.py
+++ pyprimes.py
@@ -1,13 +1,19 @@
 #author: Esteban Salpeter
 #author's email: esteban.salpeter@yahoo.com.ar
-def primes_between(min,max):
+from math import sqrt
+def primes_between(min,max,strategy=0):
 #init
+        #strategy: 0=looping and finding, other number=set.difference (consumes more memory)
         #parameters check
         if min>max:
                 print ('Error: min parameter must be lower than the max one')
                 return 0
+        if min%10 in [2,4,6,8,0]:
+                min+=1
+        if max%10 in [2,4,6,8,0]:
+                max-=1
         #the use of a set prevents duplicates
-        factorized=set()
+        nonprimeodds=set()
         #method
         #we'll produce all odd numbers in range
         #then we'll produce all non-prime odd numbers in range
@@ -20,39 +26,50 @@ def primes_between(min,max):
         #mn: minimum value of n to produce min (whatever values for m and l)
         
         #let's make only the necessary range by factorization
-        mx,mn = int((max-1)/2), int((min-1)/2)
-        m=1
-        if min%10 in [2,4,6,8,0]:
-                min+=1
-        if max%10 in [2,4,6,8,0]:
-                max-=1
-                
-        alloddsinrange=set(range(min,max+1,2))
-        
-        #uncomment line below for testing purposes
-        #print(alloddsinrange)
+        #see Theory.md for reference on this range determination
+        if strategy==0:
+                max+=2
+                min-=2
+        mx,mn = int((max)/2), int(sqrt(min))
+        m=mx
 
         #let's start filling and factorizing!
-        while m<=mx:
+        while m>=mn:
                 l=1
                 while l<=m:
                         #my super formula! generator of every nonprime odd number's half-lower-bound
                         n=m*l*2+m+l
-                        if n>mx:
+                        if n*2+1>max:
                                 break
-                        if (n<=mx) & (n>=mn):
-                                factorized.add(n*2+1)
+                        if (n*2+1<=max) & (n*2+1>=min):
+                                nonprimeodds.add(n*2+1)
                         l+=1
-                m+=1
+                m-=1
                 
         #uncomment line below for testing purposes
-        #print(factorized)
+        #print(nonprimeodds)
                 
-        #let's use the best of Python collections!
-        #use this line if you want to stay using sets instead of lists
-        primes=alloddsinrange.difference(factorized)
-        #primes=sorted(alloddsinrange.difference(factorized))
-        return primes
+        if strategy==0:
+                primes=set()
+                nonprimeodds=sorted(nonprimeodds)
+                #uncomment for testing
+                #print(nonprimeodds)
+                for i in range(0,len(nonprimeodds)-2):
+                        gap= nonprimeodds[i+1]-nonprimeodds[i]
+                        for y in range(1,int(gap/2)):
+                                primes.add(nonprimeodds[i]+2*y)
+                
+        else:
+                alloddsinrange=set(range(min,max+1,2))
+        
+                #uncomment line below for testing purposes
+                #print(alloddsinrange)
+                #let's use the best of Python collections!
+                #use this line if you want to stay using sets instead of lists
+                primes=alloddsinrange.difference(nonprimeodds)
+                #primes=sorted(alloddsinrange.difference(nonprimeodds))
+                
+        return sorted(primes)
 
 def is_prime(num):
     last = num%10
@@ -60,7 +77,7 @@ def is_prime(num):
         return True
     if last in [2,4,5,6,8,0]:
         return False
-    p=primes_between(num-1,num+1)
+    p=primes_between(num-1,num+1,1)
     if num in p:
         return True
     return False
