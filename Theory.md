2d34529e25fc8037a4a79194b01d5f6ed790adb9
Esteban <44044253+EstebanSalpeter@users.noreply.github.com>
2019-01-21 00:47:05 -0300

    Simplified the maths, polished the two strategies so they work well. It's not fast at all, but 100% accurate!! (In your face Erathostenes!)

---
+++ Theory.md
@@ -0,0 +1,45 @@
+# PyPrimes - Theory
+This is part of the package PyPrimes by Esteban Salpeter
+autor's email: esteban.salpeter@yahoo.com.ar
+
+This document is intended to support theoretically the algorithms implemented
+in the pyprimes.py functions.
+
+The main idea in this approach is that assuming we cannot generate prime
+numbers, we actually can generate non primes; more precisely, non prime odds.
+Then, analysing the resulting range of non prime odds generated, there should be
+"gaps", that is to say, differences between consecutive elements that exceed the
+expected of 2 for two consecutive odds. Those "gaps" ARE PRIMES with 100%
+accuracy.
+
+The resulting algorithm will not be probably the most efficient, but its
+strenght lies in the accuracy: it's 100%, doesn't require the "services" of 
+probabilistics. Specially when calculating large prime numbers, the accuracy
+remains 100%.
+
+Let's start.
+
+Based on the Fundamental Theorem of Aritmetics, it can be said that:
+given the ordered pair (a,b) with both belonging to Naturals, the internal
+product of the elements, a times b, may be considered as generator for every
+non-prime natural number.
+
+If we reduce to the subset of odd numbers, it's trivial that any non-prime odd 
+number must be factorized in odd numbers as well (as product will produce an 
+even whenever at least one factor is even):
+q=a*b
+This is the generator for every non prime odd number q.
+
+We want to produce the range q(min)<=q<=q(max) with all possible combinations
+of a and b's without ommiting any value in range.
+We've got a starting point at a=3, to avoid ommissions, increasing its value
+by steps of 2.
+We'll do the same for the b's, starting from q(max/3) and decreasing until:
+b=3 or b=a (it's an ordered pair, b>=a) or q results out of range.
+
+So we obtain the subset of non prime odd numbers, and we must check for the
+gaps in one of two ways:
+Strategy 0: looping and checking difference between consecutive elements.
+Strategy 1: creating another set with all odd numbers in range, and then
+obtaining the difference using that method of the set object (this is why we
+love Python!).
--- pyprimes.py
+++ pyprimes.py
@@ -1,75 +1,91 @@
 #author: Esteban Salpeter
 #author's email: esteban.salpeter@yahoo.com.ar
-from math import sqrt
 def primes_between(min,max,strategy=0):
 #init
-        #strategy: 0=looping and finding, other number=set.difference (consumes more memory)
-        #parameters check
-        if min>max:
-                print ('Error: min parameter must be lower than the max one')
-                return 0
-        if min%10 in [2,4,6,8,0]:
-                min+=1
-        if max%10 in [2,4,6,8,0]:
-                max-=1
-        #the use of a set prevents duplicates
-        nonprimeodds=set()
-        #method
-        #we'll produce all odd numbers in range
-        #then we'll produce all non-prime odd numbers in range
-        #finally, we'll obtain all prime numbers by difference of both sets
-        
-        #definitions
-        #n: half-lower-bound of a non-prime odd number
-        #m, l: ordered pair (m>=l) used in my algorithm, derived from the assumption of the form (2*n+1) for any odd non-prime number
-        #mx: maximum value for m and l to produce max when m=l
-        #mn: minimum value of n to produce min (whatever values for m and l)
-        
-        #let's make only the necessary range by factorization
-        #see Theory.md for reference on this range determination
-        if strategy==0:
-                max+=2
-                min-=2
-        mx,mn = int((max)/2), int(sqrt(min))
-        m=mx
+    #see Theory.md for reference and terminology
+    #strategy: 0=looping and finding, other number=set.difference (consumes more memory, expected to be faster)
+    #parameters check
+    if min>max:
+        print ('Error: min parameter must be lower than the max one')
+        return 0
+    if min%10 in [2,4,6,8,0]:
+        min+=1
+    if max%10 in [2,4,6,8,0]:
+        max-=1
+    #the use of a set prevents duplicates
+    nonprimeodds=set()
+    
+    #definitions
+    #n: half-lower-bound of a non-prime odd number
+    #q: a non-prime odd number
+    #a, b: any odd non-prime number as factors of q
 
-        #let's start filling and factorizing!
-        while m>=mn:
-                l=1
-                while l<=m:
-                        #my super formula! generator of every nonprime odd number's half-lower-bound
-                        n=m*l*2+m+l
-                        if n*2+1>max:
-                                break
-                        if (n*2+1<=max) & (n*2+1>=min):
-                                nonprimeodds.add(n*2+1)
-                        l+=1
-                m-=1
-                
-        #uncomment line below for testing purposes
-        #print(nonprimeodds)
-                
-        if strategy==0:
-                primes=set()
-                nonprimeodds=sorted(nonprimeodds)
-                #uncomment for testing
-                #print(nonprimeodds)
-                for i in range(0,len(nonprimeodds)-2):
-                        gap= nonprimeodds[i+1]-nonprimeodds[i]
-                        for y in range(1,int(gap/2)):
-                                primes.add(nonprimeodds[i]+2*y)
-                
-        else:
-                alloddsinrange=set(range(min,max+1,2))
+    #the range must be expanded since we'll look for gaps up to 2 primes, equals 4. We need non prime boundaries
+    if strategy==0:
+        max+=4
+        min-=4
+
+    #let's make only the necessary range by factorization
+    a=3
+    #let's start filling and factorizing!
+    while a<=int(max/3+1):
+        b=int(max/a)
+        if b%10 in [2,4,6,8,0]:
+            b+=1
+        while b>=int(min/a) and b>1 and b>=a:
+            #my super formula! 
+            q=a*b
+            #print(a,b,q)
+            if q<min:
+                break
+            if q<=max:
+                nonprimeodds.add(q)
+            b-=2
+        a+=2
+
+    #uncomment line below for testing purposes
+    #print(sorted(nonprimeodds))
         
-                #uncomment line below for testing purposes
-                #print(alloddsinrange)
-                #let's use the best of Python collections!
-                #use this line if you want to stay using sets instead of lists
-                primes=alloddsinrange.difference(nonprimeodds)
-                #primes=sorted(alloddsinrange.difference(nonprimeodds))
-                
-        return sorted(primes)
+    if strategy==0:
+    #strategy
+    #we'll check for the gaps in the nonprimeodds range
+    #there could be gaps of 4 or 6 between consecutive odds, meaning there is one or there are two primes there
+        primes=set()
+        nonprimeodds=sorted(nonprimeodds)
+        #uncomment for testing
+        #print(nonprimeodds)
+        #loop from the first up to one before the last (base 0)
+        for i in range(0,len(nonprimeodds)-1):
+            gap= nonprimeodds[i+1]-nonprimeodds[i]
+            for y in range(1,int(gap/2)):
+                primes.add(nonprimeodds[i]+2*y)
+            if min in primes:
+                primes.remove(min)
+            if max in primes:
+                primes.remove(max)
+            if min+2 in primes:
+                primes.remove(min+2)
+            if max-2 in primes:
+                primes.remove(max-2)
+        
+    else:
+    #strategy
+    #we'll produce all odd numbers in range
+    #then we'll produce all non-prime odd numbers in range
+    #finally, we'll obtain all prime numbers by difference of both sets
+        alloddsinrange=set(range(min,max+1,2))
+    
+        #uncomment line below for testing purposes
+        #print(alloddsinrange)
+        #let's use the best of Python collections!
+        #use this line if you want to stay using sets instead of lists
+        primes=alloddsinrange.difference(nonprimeodds)
+        #primes=sorted(alloddsinrange.difference(nonprimeodds))
+
+    #uncomment for testing
+    #print(len(primes))
+    #return sorted(primes)
+    return primes
 
 def is_prime(num):
     last = num%10
